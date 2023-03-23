f46528ca2c7d237051abc4b4961461571aa203d1
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-23 23:48:56 +0100

    Added loose / win screens

---
+++ NursingHome/Assets/_NursingHome/Scripts/Systems.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NursingHome
+{
+    public class Systems : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Create UI system
+        }
+    }
+}
---
+++ NursingHome/Assets/_NursingHome/Scripts/UI.cs
@@ -0,0 +1,32 @@
+﻿namespace NursingHome
+{
+    public interface IUI
+    {
+        void ShowScreen(UIType typeToShow);
+        void HideScreen(UIType typeToHide);
+    }
+
+    public enum UIType
+    {
+        WinScreen,
+        LooseScreen
+    }
+
+    public class UI : IUI
+    {
+        public UI()
+        {
+
+        }
+
+        public void HideScreen(UIType typeToHide)
+        {
+
+        }
+
+        public void ShowScreen(UIType typeToShow)
+        {
+
+        }
+    }
+}
