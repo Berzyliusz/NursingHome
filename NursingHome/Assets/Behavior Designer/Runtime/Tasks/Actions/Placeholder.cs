d381f0fc4720cd0cbc8a74d0c6b9a59d219ffe95
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-24 09:07:35 +0100

    Removed problematic Behavior Designer package

--- NursingHome/Assets/Behavior Designer/Runtime/Tasks/Actions/Placeholder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BehaviorDesigner.Runtime.Tasks
-{
-    [TaskDescription("Its a placeholder. Works as a log. This should be implemented with something.")]
-    [TaskIcon("Assets/Gizmos/BTGizmos/system-icon-placeholder.png")]
-    public class Placeholder : Log
-    {
-
-    }
-}
