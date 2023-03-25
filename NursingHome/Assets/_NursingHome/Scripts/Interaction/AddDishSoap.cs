f8be67bfb4bb82a211e7b1f154173ade53e59445
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-25 18:37:27 +0100

    stubbed classes for pranks

--- NursingHome/Assets/_NursingHome/Scripts/Interaction/AddDishSoap.cs
+++ NursingHome/Assets/_NursingHome/Scripts/Interaction/AddDishSoap.cs
@@ -7,4 +7,34 @@ namespace NursingHome.Interactions
     {
 
     }
+
+    [CreateAssetMenu(menuName = "NursingHome/Pranks/Spice up soup")]
+    public class SpiceUpSoup : PrankParams
+    {
+
+    }
+
+    [CreateAssetMenu(menuName = "NursingHome/Pranks/Tape PC Mouse")]
+    public class TapePCMouse : PrankParams
+    {
+
+    }
+
+    [CreateAssetMenu(menuName = "NursingHome/Pranks/Cut Table Leg")]
+    public class CutTableLeg : PrankParams
+    {
+
+    }
+
+    [CreateAssetMenu(menuName = "NursingHome/Pranks/Clog With Tampons")]
+    public class ClogWithTampons : PrankParams
+    {
+
+    }
+
+    [CreateAssetMenu(menuName = "NursingHome/Pranks/Smash With Hammer")]
+    public class SmashWithHammer : PrankParams
+    {
+
+    }
 }
