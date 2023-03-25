c12008f14461ff99d0206598d911b09aa73571b8
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-25 15:19:24 +0100

    Made items code more generic, replugged pickup items

--- NursingHome/Assets/_NursingHome/Scripts/Interaction/InteractableItem.cs
+++ NursingHome/Assets/_NursingHome/Scripts/Interaction/InteractableItem.cs
@@ -1,22 +1,12 @@
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace NursingHome.Interactions
 {
     [RequireComponent(typeof(Collider))]
-    public class InteractableItem : MonoBehaviour
+    public abstract class InteractableItem : MonoBehaviour
     {
-        public string DisplayName => itemParams.ItemName;
-        public ItemParams ItemParams => itemParams;
-
-        [SerializeField]
-        [InlineEditor]
-        ItemParams itemParams;
-
-        public void Pickup()
-        {
-            gameObject.SetActive(false);
-            Destroy(gameObject, 1f);
-        }
+        public abstract string DisplayName { get; }
+        public abstract ItemParams ItemParams { get; }
+        public abstract void UseItem();
     }
 }
