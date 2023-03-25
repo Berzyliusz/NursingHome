d030a157cbc84c43a1198f42b1020847e3e346b6
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-25 17:33:57 +0100

    Replugged params

--- NursingHome/Assets/_NursingHome/Scripts/Interaction/ItemParams.cs
+++ NursingHome/Assets/_NursingHome/Scripts/Interaction/ItemParams.cs
@@ -2,14 +2,11 @@
 
 namespace NursingHome.Interactions
 {
-    [CreateAssetMenu(menuName = "NursingHome/ItemParams")]
-    public class ItemParams : ScriptableObject
+    public abstract class ItemParams : ScriptableObject
     {
         [field:SerializeField] 
         public string ItemName { get; private set; }
 
-        // Hold params for:
-        // What pranks are available with this item?
-        // Severity of this iteam being found upon us
+
     }
 }
