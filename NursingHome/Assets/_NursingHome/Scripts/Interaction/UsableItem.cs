4cd04bb3d0d0c1a18d96addc8e9288a1be447521
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-25 15:25:39 +0100

    Plugged usable item script to usables

---
+++ NursingHome/Assets/_NursingHome/Scripts/Interaction/UsableItem.cs
@@ -0,0 +1,14 @@
+﻿namespace NursingHome.Interactions
+{
+    public class UsableItem : InteractableItem
+    {
+        public override string DisplayName => throw new System.NotImplementedException();
+
+        public override ItemParams ItemParams => throw new System.NotImplementedException();
+
+        public override void UseItem()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
