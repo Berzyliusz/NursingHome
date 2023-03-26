9800c887681404a9da6fc87902054cbd90cc9097
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-26 11:22:45 +0200

    Added player movement freezing

--- NursingHome/Assets/_NursingHome/Scripts/Systems.cs
+++ NursingHome/Assets/_NursingHome/Scripts/Systems.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using NursingHome.UserInterface;
 using NursingHome.Interactions;
+using StarterAssets;
+using Sirenix.OdinInspector;
 
 namespace NursingHome
 {
+
     public class Systems : MonoBehaviour
     {
         public static Systems Instance { get; private set; }
@@ -14,8 +17,12 @@ namespace NursingHome
         public InteractionDetector InteractionDetector { get; private set; }
 
         [SerializeField]
+        FirstPersonController playerController;
+
+        [SerializeField]
         ItemPicker itemPicker;
 
+        public IPlayer Player { get; private set; }
         public IInputs Inputs { get; private set; }
 
         public PlayerInventory Inventory { get; private set; }
@@ -25,6 +32,13 @@ namespace NursingHome
             Instance = this;
             Inventory = new PlayerInventory(itemPicker);
             Inputs = new Inputs();
+            Player = playerController;
+        }
+
+        [Button]
+        void SetFreezePlayer(bool isFrozen)
+        {
+            Player.SetFreezePlayer(isFrozen);
         }
     }
 }
