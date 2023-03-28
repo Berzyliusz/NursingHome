51042a32e2a03d148ea4c2672bb97001919205ae
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-28 21:35:47 +0200

    Prepared prefabs for sitting and laying pensioners, added sit and lay animations

--- NursingHome/Assets/_NursingHome/Scripts/AI/AnimationHashes.cs
+++ NursingHome/Assets/_NursingHome/Scripts/AI/AnimationHashes.cs
@@ -8,5 +8,7 @@ namespace NursingHome.Animations
         public static int IdleHash = Animator.StringToHash("Idle");
         public static int WorkHash = Animator.StringToHash("Work");
         public static int ChaseHash = Animator.StringToHash("Chase");
+        public static int SitHash = Animator.StringToHash("Sit");
+        public static int LayHash = Animator.StringToHash("Lay");
     }
 }
