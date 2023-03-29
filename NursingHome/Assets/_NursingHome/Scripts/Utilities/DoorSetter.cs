ed8ac4d253ca677d84ec41821ea7ef5657b28eae
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-29 10:07:37 +0200

    Pivot switcher almost working

--- NursingHome/Assets/_NursingHome/Scripts/Utilities/DoorSetter.cs
+++ NursingHome/Assets/_NursingHome/Scripts/Utilities/DoorSetter.cs
@@ -1,4 +1,5 @@
 using Sirenix.OdinInspector;
+using System;
 using UnityEngine;
 
 namespace Utilities
@@ -25,28 +26,83 @@ namespace Utilities
         [FoldoutGroup("Door Params")]
         Transform outwardsRightPivot;
 
+        [SerializeField]
+        [FoldoutGroup("Door Params")]
+        Transform inwardsLeftPivot;
+
+        [SerializeField]
+        [FoldoutGroup("Door Params")]
+        Transform outwardsLeftPivot;
+
+
         [Button]
         void SetDoorPivot(DoorType doorType)
         {
-            if(doorType == DoorType.RightInwards)
+            DisablePivot(inwardsRightPivot);
+            DisablePivot(outwardsLeftPivot);
+            DisablePivot(outwardsLeftPivot);
+            DisablePivot(inwardsLeftPivot);
+
+            if (doorType == DoorType.RightInwards)
             {
                 if (!inwardsRightPivot)
                     return;
 
+                inwardsRightPivot.localEulerAngles = Vector3.zero;
                 doors.parent = inwardsRightPivot;
                 doors.localPosition = Vector3.zero;
                 doors.localEulerAngles = Vector3.zero;
+                inwardsRightPivot.name = "DOOR " + inwardsRightPivot.name;
+                inwardsRightPivot.gameObject.SetActive(true);
             }
 
-            if(doorType == DoorType.RightOutwards)
+            if (doorType == DoorType.RightOutwards)
             {
                 if (!outwardsRightPivot)
                     return;
 
-                doors.parent = outwardsRightPivot;
+                outwardsRightPivot.localEulerAngles = Vector3.zero;
+                doors.parent = inwardsRightPivot;
                 doors.localPosition = Vector3.zero;
                 doors.localEulerAngles = Vector3.zero;
+                doors.parent = outwardsRightPivot;
+                outwardsRightPivot.name = "DOOR " + outwardsRightPivot.name;
+                outwardsRightPivot.gameObject.SetActive(true);
             }
+
+            if (doorType == DoorType.LeftOutwards)
+            {
+                if (!outwardsLeftPivot)
+                    return;
+
+                outwardsLeftPivot.localEulerAngles = new Vector3(0, 180, 0);
+                doors.parent = inwardsLeftPivot;
+                doors.localPosition = Vector3.zero;
+                doors.localEulerAngles = Vector3.zero;
+                doors.parent = outwardsLeftPivot;
+                outwardsLeftPivot.name = "DOOR " + outwardsLeftPivot.name;
+                outwardsLeftPivot.gameObject.SetActive(true);
+
+            }
+
+            if (doorType == DoorType.LeftInwards)
+            {
+                if (!inwardsLeftPivot)
+                    return;
+
+                inwardsLeftPivot.localEulerAngles = new Vector3(0, 180, 0);
+                doors.parent = inwardsLeftPivot;
+                doors.localPosition = Vector3.zero;
+                doors.localEulerAngles = Vector3.zero;
+                inwardsLeftPivot.name = "DOOR " + inwardsLeftPivot.name;
+                inwardsLeftPivot.gameObject.SetActive(true);
+            }
+        }
+
+        void DisablePivot(Transform pivotToDisable)
+        {
+            pivotToDisable.name = inwardsRightPivot.name.Replace("DOOR ", string.Empty);
+            pivotToDisable.gameObject.SetActive(false);
         }
     }
 }
