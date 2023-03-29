0405cc7e166c661c3460181c00768747b3ce3e23
DESKTOP-LRU7KSA\Krzysiek <k.poros@gmail.com>
2023-03-29 17:51:14 +0200

    Stubbed out classes are shit

--- NursingHome/Assets/_NursingHome/Scripts/Audio/BackgroundMusicPlayer.cs
+++ NursingHome/Assets/_NursingHome/Scripts/Audio/BackgroundMusicPlayer.cs
@@ -21,7 +21,7 @@ namespace NursingHome.Audio
         float cooldownTimer = 0;
 
         IGameStateDispatcher gameStateDispatcher;
-        BackgroundMusicType musicType;
+        BackgroundMusicType currentMusicType;
 
         public BackgroundMusicPlayer(IGameStateDispatcher gameStateDispatcher)
         {
@@ -32,40 +32,48 @@ namespace NursingHome.Audio
             gameStateDispatcher.OnPlayerChased += HandlePlayerChased;
             gameStateDispatcher.OnPrankFound += HandlePrankFound;
 
-            musicType = BackgroundMusicType.Exploration;
+            SwitchMusicTo(BackgroundMusicType.Exploration);
         }
 
         public void Update(float deltaTime)
         {
-            cooldownTimer -= deltaTime;
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("Playing music: " + currentMusicType);
+            }
 
-            // if cooldown timer below zero, go lower
+            if (currentMusicType == BackgroundMusicType.Exploration)
+                return;
+
+            cooldownTimer -= deltaTime;
+            if(cooldownTimer <= 0 )
+            {
+                currentMusicType = (BackgroundMusicType)Mathf.Max((int)currentMusicType--, 0);
+                SwitchMusicTo(currentMusicType);
+            }
         }
 
         void HandlePlayerChased()
         {
-            if(musicType != BackgroundMusicType.Chase)
+            if(currentMusicType != BackgroundMusicType.Chase)
             {
-                musicType = BackgroundMusicType.Chase;
-
-                SwitchMusicTo(musicType);
+                SwitchMusicTo(BackgroundMusicType.Chase);
             }
         }
 
         void HandlePrankFound()
         {
-            if(musicType != BackgroundMusicType.Searching)
+            if(currentMusicType == BackgroundMusicType.Exploration)
             {
-                musicType = BackgroundMusicType.Searching;
-
-                SwitchMusicTo(musicType);
-
+                SwitchMusicTo(BackgroundMusicType.Searching);
             }
         }
 
         void SwitchMusicTo(BackgroundMusicType musicType)
         {
+            Debug.Log("Switching music from: " + currentMusicType + " to: " + musicType);
             cooldownTimer = musicCooldownTime;
+            currentMusicType = musicType;
 
             // Fade out previous music
             // Fade in new music
